f3891ee68fb90032d9fb34a901e4a52f8235c6dd
Derek Bourgeois <derek@ibourgeois.com>
2014-11-15 23:33:42 -0500

    Update filters.php

--- src/filters.php
+++ src/filters.php
@@ -1,38 +1,6 @@
 <?php
 
-Route::filter('validate_dev', function ()
+Route::filter('laraedit_dev', function ()
 {
-	/*
-
-	$configFactory = App::make('admin_config_factory');
-
-	//get the admin check closure that should be supplied in the config
-	$permission = Config::get('administrator::administrator.permission');
-	$response = $permission();
-
-	//if this is a simple false value, send the user to the login redirect
-	if (!$response)
-	{
-		$loginUrl = URL::to(Config::get('administrator::administrator.login_path', 'user/login'));
-		$redirectKey = Config::get('administrator::administrator.login_redirect_key', 'redirect');
-		$redirectUri = Request::url();
-
-		return Redirect::guest($loginUrl)->with($redirectKey, $redirectUri);
-	}
-	//otherwise if this is a response, return that
-	else if (is_a($response, 'Illuminate\Http\JsonResponse') || is_a($response, 'Illuminate\Http\Response'))
-	{
-		return $response;
-	}
-	//if it's a redirect, send it back with the redirect uri
-	else if (is_a($response, 'Illuminate\\Http\\RedirectResponse'))
-	{
-		return $response->with($redirectKey, $redirectUri);
-	}
-
-	*/
-
-	
-
 
 });
