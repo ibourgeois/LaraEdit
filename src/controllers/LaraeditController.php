07a516f65a18e981e7ad2c16403f92519a8ca4e7
Derek Bourgeois <derek@ibourgeois.com>
2014-11-15 23:50:21 -0500

    Update LaraeditController.php

--- src/controllers/LaraeditController.php
+++ src/controllers/LaraeditController.php
@@ -1,6 +1,6 @@
 <?php namespace Ibourgeois\Laraedit;
 
-class LaraeditController extends Controller
+class LaraeditController extends \Controller
 {
     public function getIndex() {
         return 'LaraEdit is working...';
