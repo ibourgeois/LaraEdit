18ff4f06855415c9c65185407c1df25cf31e5930
Derek Bourgeois <derek@ibourgeois.com>
2014-11-16 00:47:44 -0500

    Added Dynamic Page Title

--- src/views/layout/master.blade.php
+++ src/views/layout/master.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     
-        <title>LaraEdit</title>
+        <title>{{ Config::get('laraedit::laraedit.title') }}</title>
 
         <link rel="stylesheet" href="{{asset('packages/ibourgeois/laraedit/css/bootstrap/bootstrap.min.css')}}">
         <link rel="stylesheet" href="{{asset('packages/ibourgeois/laraedit/css/font-awesome/font-awesome.min.css')}}">
