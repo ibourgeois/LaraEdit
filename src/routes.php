bb2647357a59233c4a83eab12be2e2af168925fa
Derek Bourgeois <derek@ibourgeois.com>
2014-11-15 23:49:55 -0500

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,9 @@
 <?php
 
-Route::group(array('prefix' => Config::get('laraedit::laraedit.uri'), 'before' => 'laraedit_dev'), function()
+Route::group(array('prefix' => Config::get('laraedit::laraedit.uri')), function()
 {
 	Route::get('/', array(
 		'as' => 'laraedit_home',
 		'uses' => 'Ibourgeois\Laraedit\LaraeditController@getIndex'
 	));
-};
+});
